9eaa34c0a192c6b1f8925c1727425a61b8941d89
Jhonny Neciosup Millones <jhonny.milones@gmail.com>
2019-08-23 16:34:59 -0500

    Aprendiendo nuxt

---
+++ components/AlbumCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="column is-one-quarter">
+    <div class="box">
+      <h3>{{album.title}}</h3>
+      <a href="#">ver más</a>
+    </div>
+      
+  </div>
+</template>
+<script>
+export default {
+  name:'AlbumCard',
+  props:{
+    album: Object
+  }
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h1 class="title">Aqui viene la lista de Albums</h1>
+    <div >
+      <div class="columns is-multiline">
+        <AlbumCard :album="album" v-for="album in albums" :key="album.id"></AlbumCard>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import env from '../config/env';
+import AlbumCard from '../components/AlbumCard'
+export default {
+  name: 'IndexPage',
+  components:{
+    AlbumCard
+  },
+  data() {
+    return {
+      mensaje:'',
+      albums:[]
+    }
+  },
+  created (){
+    axios.get(`${env.endpoint}/albums`).then(res => {
+      this.albums = res.data;
+      console.log(res);
+    })
+  }
+  
+}
+</script>
+<style scoped>
+  
+</style>
+/* https://codigofacilito.com/videos/integrar-un-framework-css */
